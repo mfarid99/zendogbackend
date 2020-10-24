72a03b5e0e499f21cf9eb76c011205a28e5bde35
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-10-24 11:30:12 -0400

    dogs backend

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ const dogRouter = require("./controllers/dog");
 ////////////
 //MIDDLEWARE
 ////////////
-NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+// NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny")); //logging
+app.use(cors())
 
 ///////////////
 //Routes and Routers
